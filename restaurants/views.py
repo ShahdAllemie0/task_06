91573faf4c71bf5fb00505a7c3d6baa9b19f13d3
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-08-31 13:21:01 +0300

    done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RestaurantForm
 from .models import Restaurant
 
 def welcome(request):
@@ -18,8 +19,24 @@ def restaurant_detail(request, restaurant_id):
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
+    form =RestaurantForm()
+    if request.method == "POST":
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("restaurant-list")
 
     context = {
-
-    }
+    "form":form,
+     }
     return render(request, 'create.html', context)
+# Create a form for creating Restaurant objects.
+# Your form should be in a forms.py file.
+# Your form should be called RestaurantForm.
+# Your form should include all of the fields.
+# Complete the restaurant_create view so that it can be used to allow users to create Restaurant objects.
+# After successfully creating an object the user should be redirected to another page.
+# Complete the HTML file provided, so that the user input can be taken and handled properly.
+# The URL has been written for you so you don't need to create one.
+# Pass the tests.
+# Push your code.
